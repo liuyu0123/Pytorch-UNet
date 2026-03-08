c39561a8542e7b5e9c822e60e83cdc9e6cffca65
liuyu <liuyu0123@outlook.com>
2026-03-08 12:11:22 +0800

    modify run.sh, how to predict a img

--- run.sh
+++ run.sh
@@ -4,3 +4,9 @@ python train.py --amp
 
 #训练模型（禁用wandb）
 python train_disable_wandb.py --amp
+
+#测试模型
+#测试并保存结果
+python predict.py --model ./checkpoints/checkpoint_epoch5.pth -i "D:\Files\Data\USVInlandDataset\Water Segmentation\training\training\640_320_undistorted\H05_1_0000000000.jpg" -o output.jpg
+#测试不保存结果（仅展示）
+python predict.py --model ./checkpoints/checkpoint_epoch5.pth -i "D:\Files\Data\USVInlandDataset\Water Segmentation\training\training\640_320_undistorted\H05_1_0000000000.jpg" --viz --no-save
